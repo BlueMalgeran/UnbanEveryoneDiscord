21bdca02eb5a95fe9d6f1cff6553acee5836d38e
Blue Malgeran <yos7816@gmail.com>
2018-10-04 20:39:59 +0300

    ez noobs

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client } = require('discord.js');
+const client = new Client();
+const prefix = '!';
+const ownerID = 'your id'
+
+client.on('ready', () => console.log('Ready!'));
+
+client.on('message',  async msg => {
+    if(msg.author.id != ownerID) return msg.channel.send('only the owner of the bot can use this command.');
+
+    if(msg.content.startsWith(`${prefix}unbanall`)) {
+        msg.guild.fetchBans()
+        .then(bans => {
+            bans.forEach(user => {
+                /*
+                Debug works
+                console.log(user.id);
+                */
+                msg.guild.unban(user.id);
+            });
+        })
+        .catch(e => console.log(e));
+    }
+});
+
+client.login('TOKEN');
